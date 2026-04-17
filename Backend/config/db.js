b7d0e6f241d55570c73d831dad99a3fa1a8c0b0e
HP <HP@DESKTOP-C1V34EL>
2026-04-17 15:48:04 +0530

    change in url

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB=async()=>{
-    await mongoose.connect("mongodb+srv://Sakshi_1234:Sakshi2005@cluster1.ta0p3sv.mongodb.net/?appName=Cluster1/ExpnseTracker")
+    await mongoose.connect("mongodb+srv://Sakshi_1234:Sakshi2005@cluster1.ta0p3sv.mongodb.net/ExpenseTracker")
     .then(()=> console.log("Connected to MongoDB"));
 
 }
