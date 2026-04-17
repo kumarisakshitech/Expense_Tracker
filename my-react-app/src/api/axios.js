0bcbc163058af2fbe3f6a37e022706f66d18deae
HP <HP@DESKTOP-C1V34EL>
2026-04-17 15:01:20 +0530

    update api

--- my-react-app/src/api/axios.js
+++ my-react-app/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://expense-tracker-backend-pbjj.onrender.com/api',
 });
 
 // Attach token to every request
